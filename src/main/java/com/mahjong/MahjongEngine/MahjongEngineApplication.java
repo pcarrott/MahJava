8e703f0781acfac86cd2a4e4c281a46639b79a32
Carrott99 <pedro.carrott.99@gmail.com>
2021-02-25 17:12:09 +0000

    Initial commit

---
+++ src/main/java/com/mahjong/MahjongEngine/MahjongEngineApplication.java
@@ -0,0 +1,13 @@
+package com.mahjong.MahjongEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahjongEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahjongEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahjong/MahjongEngine/MahjongEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahjong.MahjongEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahjongEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
